2f02967bfe9256c5a8f65adc2d15855d3c3e7676
Ethan Teague <ethan.teague@civicactions.com>
2017-05-07 22:39:30 -0400

    Ready for production

--- assets/scripts/astroads.js
+++ assets/scripts/astroads.js
@@ -1,5 +1,5 @@
-
-var containerTwo = '<div class="astro-ads-container1 astro-ads-container astro-ads-container-second">\
+// Add set one.
+var containerOne = '<div class="astro-ads-container1 astro-ads-container astro-ads-container-second">\
 <span class="astro-ads-container-tag-left">\
 <div style="clear:both"></div>\
 </span>\
@@ -40,7 +40,8 @@ var containerTwo = '<div class="astro-ads-container1 astro-ads-container astro-a
 </a>\
 </div>';
 
-var containerOne = '<div class="astro-ads-container1 astro-ads-container">\
+// Add set two.
+var containerTwo = '<div class="astro-ads-container1 astro-ads-container">\
 <span class="astro-ads-container-tag-left">\
 <div style="clear:both"></div>\
 </span>\
@@ -81,11 +82,43 @@ var containerOne = '<div class="astro-ads-container1 astro-ads-container">\
 </a>\
 </div>';
 
-var data = '<div class="astro-ads-container-wrap">' +
-  '<h3 class="astro-ads-container-tag-header astro-ads-container-tag-collection">MTrue\'s Picks</h3>' +
+// Determine odd or even counter.
+function isEven(value) {
+  if (value % 2 == 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+// Change out our items every few seconds.
+var counter = 0;
+setInterval(change, 3000);
+function change() {
+  if (isEven(counter)) {
+    document.getElementById('astro-ads-target').innerHTML = '<h3 id="astro-header-set" class="astro-ads-container-tag-header astro-ads-container-tag-collection">MTrue\'s Picks</h3>' +
+      containerOne +
+      '<div style="clear:both"></div>';
+  }
+  else {
+    document.getElementById('astro-ads-target').innerHTML = '<h3 id="astro-header-set" class="astro-ads-container-tag-header astro-ads-container-tag-collection">MTrue\'s Picks</h3>' +
+      containerTwo +
+      '<div style="clear:both"></div>';
+  }
+
+  counter++;
+}
+
+// Plug our add html into var.
+var data = '<div id="astro-ads-target" class="astro-ads-container-wrap"><h3 id="astro-header-set" class="astro-ads-container-tag-header astro-ads-container-tag-collection">MTrue\'s Picks</h3>' +
   containerTwo +
   '<div style="clear:both"></div></div>';
+if (document.getElementById('astro-header-set') != null) {
+  var data = '<div id="astro-ads-target" class="astro-ads-container-wrap"></div>';
+}
 
+// Plug our add html into dom.
 window.onload = function () {
   var targetItem = document.getElementById('astro-ads');
   targetItem.innerHTML = data;
